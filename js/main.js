fdfa1bd59219d8df106a16b23054bfe49dd997da
Jeanne Ma <jeanne.ma.3@gmail.com>
2014-12-04 23:54:08 -0500

    google maps api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 // main.js
+var currLat, currLong;
+
+$( document ).ready(function(){
+
+    // initialize map
+    var mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(40.7993,-73.9667)
+    }
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var current = new google.maps.Marker({
+        position: new google.maps.LatLng(40.7833,-73.9667),
+        map: map,
+        title: 'Central Park',
+        icon: 'img/puppy.png'
+    });
+    
+    var school = new google.maps.Marker({
+        position: new google.maps.LatLng(40.8075,-73.9619),
+        map: map,
+        title: 'School',
+        icon: 'img/seas.png'
+    });
+});
